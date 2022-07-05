7acbc1aca9a6176951581b8d284090491b5ae6ea
S-OS <33400784+sosudo@users.noreply.github.com>
2022-07-05 15:59:32 -0400

    Create main.py

---
+++ USACO/Bronze/2017/December/Problem 3. Milk Measurement/main.py
@@ -0,0 +1,38 @@
+import sys
+sys.stdin = open('measurement.in', 'r')
+sys.stdout = open('measurement.out', 'w')
+bessie = [7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7]
+elsie = [7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7]
+mildred = [7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7]
+N = int(input())
+for i in range(N):
+	x = input().split(" ")
+	index = int(x[0])
+	name = x[1]
+	delta = int(x[2])
+	if name == 'Mildred':
+		for j in range(index, 100):
+			mildred[j] += delta
+	elif name == 'Bessie': 
+		for j in range(index, 100):
+			bessie[j] += delta
+	elif name == 'Elsie':
+		for j in range(index, 100):
+			elsie[j] += delta
+	else:
+		pass
+ans = 0
+pred = [1, 1, 1]
+display = [1, 1, 1]
+for i in range(100):
+	m = mildred[i]
+	e = elsie[i]
+	b = bessie[i]
+	_max = max(m, e, b)
+	display[0] = 1 if b == _max else 0
+	display[1] = 1 if e == _max else 0
+	display[2] = 1 if m == _max else 0
+	if pred != display:
+		ans += 1
+	pred = [i for i in display]
+print(ans)
