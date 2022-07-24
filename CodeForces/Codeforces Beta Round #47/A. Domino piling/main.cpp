69c721d4248ee3c847a055b5705a280e2b274efc
S-OS <33400784+sosudo@users.noreply.github.com>
2022-07-24 18:11:54 -0400

    Create main.cpp

---
+++ CodeForces/Codeforces Beta Round #47/A. Domino piling/main.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+#define println(x) cout << x << "\n"
+#define input(x) cin >> x
+using namespace std;
+int main() {
+	int m, n;
+	input(m);
+	input(n);
+	println((m*n)/2);
+}
